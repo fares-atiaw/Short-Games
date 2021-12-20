8a9a222dfb07f7c9e94274aa7539b7b3f86793da
fares <74656933+fares-atiaw@users.noreply.github.com>
2021-12-20 15:13:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def welcome_choice():
           "Press [1] => Even / Odd Game \n"
           "Press [2] => Sum & Average Game \n"
           "Press [3] => Multiplication Game \n"
+          "Press [4] => Power numbers \n"
+          "Press [5] => Where is my item ? \n"
+          "Press [6] => I like Even numbers only ! \n"
+          "Press [7] => I like Odd numbers only ! \n"
           " ... When you wanna quit, just write (123)\n")
 
 
@@ -19,6 +23,14 @@ def choices():
                 Game.Sum_Average_Game()
             elif x == '3':
                 Game.Multiplication_Game()
+            elif x == '4':
+                Game.Power_Numbers()
+            elif x == '5':
+                Game.Where_is_my_item()
+            elif x == '6':
+                Game.Get_Even_Numbers()
+            elif x == '7':
+                Game.Get_Odd_Numbers()
             elif x == '123':
                 print(f'We hope you have enjoyed. Salam !')
                 break
